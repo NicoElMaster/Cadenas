ef58ee075bdb02aa5acbffc54d082d4ca1f54702
Nicolas Moreno Jiménez <102886853+NicoElMaster@users.noreply.github.com>
2024-11-18 13:14:35 -0500

    Add files via upload

---
+++ Mavenproject1.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author NMorenoJ
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
